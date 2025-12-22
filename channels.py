305017c28f9ce5df897b92d7fc2df942860de812
smtv62 <fckfacebook@hotmail.com.tr>
2025-12-22 22:47:07 +0300

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,4 +1,9 @@
 CHANNELS = [
     {"id": "yayin1", "name": "Bein Sports 1 HD"},
     {"id": "yayinb2", "name": "Bein Sports 2 HD"},
+    {"id": "yayinb3", "name": "Bein Sports 3 HD"},
+    {"id": "yayinb4", "name": "Bein Sports 4 HD"},
+    {"id": "yayinb5", "name": "Bein Sports 5 HD"},
+    {"id": "yayinss", "name": "S Sport HD"},
+    {"id": "yayintrtspor", "name": "TRT Spor HD"},
 ]
