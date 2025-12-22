37eee6e7b17534a95fe9f76ec7d0d690e706bc1f
smtv62 <fckfacebook@hotmail.com.tr>
2025-12-22 22:17:07 +0300

    Delete src directory

--- src/cache.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-CACHE_FILE = Path(".last_domain")
-
-def load_cached_domain():
-    return CACHE_FILE.read_text().strip() if CACHE_FILE.exists() else None
-
-def save_cached_domain(domain):
-    CACHE_FILE.write_text(domain)
