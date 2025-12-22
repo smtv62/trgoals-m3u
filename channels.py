642bf9ef4ff3ee0c91103c0b6fb2789fedd33c49
smtv62 <fckfacebook@hotmail.com.tr>
2025-12-22 23:02:35 +0300

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,9 +1,9 @@
 CHANNELS = [
-    {"id": "yayin1", "name": "Bein Sports 1 HD"},
-    {"id": "yayinb2", "name": "Bein Sports 2 HD"},
-    {"id": "yayinb3", "name": "Bein Sports 3 HD"},
-    {"id": "yayinb4", "name": "Bein Sports 4 HD"},
-    {"id": "yayinb5", "name": "Bein Sports 5 HD"},
-    {"id": "yayinss", "name": "S Sport HD"},
-    {"id": "yayintrtspor", "name": "TRT Spor HD"},
+    {"id": "yayin1",  "file": "yayin1.m3u8",  "name": "Bein Sports 1 HD"},
+    {"id": "yayinb2", "file": "yayinb2.m3u8", "name": "Bein Sports 2 HD"},
+    {"id": "yayinb3", "file": "yayinb3.m3u8", "name": "Bein Sports 3 HD"},
+    {"id": "yayinb4", "file": "yayinb4.m3u8", "name": "Bein Sports 4 HD"},
+    {"id": "yayinb5", "file": "yayinb5.m3u8", "name": "Bein Sports 5 HD"},
+    {"id": "yayinss", "file": "yayinss.m3u8", "name": "S Sport HD"},
+    {"id": "yayintrtspor", "file": "yayintrtspor.m3u8", "name": "TRT Spor HD"},
 ]
