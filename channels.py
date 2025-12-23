7f5b2a7d2ead58f9f0a468ef7d082a57d3180814
smtv62 <fckfacebook@hotmail.com.tr>
2025-12-23 19:06:19 +0300

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,9 +1,13 @@
 CHANNELS = [
-    {"id": "yayin1", "name": "Bein Sports 1 HD", "file": "yayin1.m3u8"},
-    {"id": "yayinb2", "name": "Bein Sports 2 HD", "file": "yayinb2.m3u8"},
-    {"id": "yayinb3", "name": "Bein Sports 3 HD", "file": "yayinb3.m3u8"},
-    {"id": "yayinb4", "name": "Bein Sports 4 HD", "file": "yayinb4.m3u8"},
-    {"id": "yayinb5", "name": "Bein Sports 5 HD", "file": "yayinb5.m3u8"},
-    {"id": "yayinss", "name": "S Sport HD", "file": "yayinss.m3u8"},
-    {"id": "yayintrtspor", "name": "TRT Spor HD", "file": "yayintrtspor.m3u8"},
+    {"id": "yayin1", "name": "Bein Sports 1 HD"},
+    {"id": "yayinb2", "name": "Bein Sports 2 HD"},
+    {"id": "yayinb3", "name": "Bein Sports 3 HD"},
+    {"id": "yayinb4", "name": "Bein Sports 4 HD"},
+    {"id": "yayinb5", "name": "Bein Sports 5 HD"},
+    {"id": "yayinss", "name": "S Sport HD"},
+    {"id": "yayintrtspor", "name": "TRT Spor HD"},
+    {"id": "yayinas", "name": "A Spor HD"},
+    {"id": "yayinatv", "name": "ATV HD"},
+    {"id": "yayintv8", "name": "TV8 HD"},
+    # Daha fazla kanal eklenebilir
 ]
